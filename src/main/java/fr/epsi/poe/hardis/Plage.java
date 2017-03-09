4d6df3c0ba64160270151fb5001501370835ff38
MariePETROD <marie.petrod@sfr.fr>
2017-03-09 08:59:38 +0100

    Commit pour classe Plage sur branche feature

--- src/main/java/fr/epsi/poe/hardis/Plage.java
+++ src/main/java/fr/epsi/poe/hardis/Plage.java
@@ -1,7 +1,25 @@
 package fr.epsi.poe.hardis;
 
 /**
- * Created by T'Chi on 09/03/2017.
+ * Plage : classe pour tester la branche feature
+ * @version : 1.0
+ * @author : Marie PÉTROD
  */
+
 public class Plage {
+    //Nom de la plage
+    private String nom;
+
+    //Nombre de parasols
+    private int nbParasol;
+
+    //Température de l'eau
+    private int temp;
+
+    //********************CONSTRUCTEURS********************//
+    public Plage(String pNom, int pNbParasol, int pTemp) {
+        nom = pNom;
+        nbParasol = pNbParasol;
+        temp = pTemp;
+    }
 }
