72d34ae335ca707fc9880aff4cf71383674d6631
olivierh63 <olivier.ferrant@free.fr>
2017-03-09 09:02:56 +0100

    feature hotel

---
+++ src/main/java/fr/epsi/poe/hardis/Hotel.java
@@ -0,0 +1,37 @@
+package fr.epsi.poe.hardis;
+
+/**
+ * Created by Olivier on 09/03/2017.
+ */
+public class Hotel {
+    private String nom;
+
+    //Adresse de l'hotel
+    private String adresse;
+
+    //Code postal l'hotel
+    private String codePostal;
+
+    /* N° de téléphone l'hotel */
+    private String telephone;
+
+    //Ouverture ou fermeture l'hotel
+    private boolean ouverture = true;
+
+    public Hotel(String nom, String adresse, String codePostal, String telephone, boolean ouverture) {
+        this.nom = nom;
+        this.adresse = adresse;
+        this.codePostal = codePostal;
+        this.telephone = telephone;
+        this.ouverture = ouverture;
+    }
+
+
+    public void setTelephone(String pTelephone) {
+        //vérifier si le téléphone n'est pas nul, n'est pas vide et commence bien par +33
+        if (pTelephone != null && !"".equals(pTelephone) && pTelephone.startsWith("+33")) {
+            telephone = pTelephone;
+        }
+    }
+
+}
