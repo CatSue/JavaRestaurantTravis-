bfa608687372af72cea01cb55267b5143890fdf8
Olivier FERRANT <olivier.ferrant@free.fr>
2017-03-15 11:29:39 +0100

    release 2 attention aux vagues # Conflicts: #	src/main/java/fr/epsi/poe/hardis/Plage.java

---
+++ src/main/java/fr/epsi/poe/hardis/Vagues.java
@@ -0,0 +1,14 @@
+package fr.epsi.poe.hardis;
+
+/**
+ * Created by Olivier on 15/03/2017.
+ */
+public class Vagues {
+    private float hauteur;
+    private float frequence;
+
+    public Vagues(float hauteur, float frequence) {
+        this.hauteur = hauteur;
+        this.frequence = frequence;
+    }
+}
